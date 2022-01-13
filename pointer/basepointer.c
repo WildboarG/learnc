8df27e7bc9d9f0a885d8e11f847d65e77190a5fe
WildboarG <959586@outlook.com>
2022-01-13 21:29:57 +0800

    指针的定义与运算

--- pointer/basepointer.c
+++ pointer/basepointer.c
@@ -4,12 +4,18 @@
 
 int main(){
 		int i=2;
-		int *p =&i;
-
+		int *p = &i;
+		int **q = &p; //二级指针
+	    //int*** m = &q;//三级指针
 		printf("i = %d\n",i);
 		printf("&i = %p\n",&i);
+		
 		printf("p = %p\n",p);
 		printf("&p = %p\n",&p);
-
+		printf("*p = %d\n",*p);
+		
+		printf("q = %p\n",q);
+		printf("*q = %p\n",*q);
+		printf("**q = %d\n",**q);
 		exit(0);
 }
---
+++ pointer/pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+
+int main(){
+	int *p = NULL;  //空指针 是为了防止野指针出现
+	void *q = NULL; //空类型可以接受任何类型的指针
+
+
+
+	return 0;
+}
