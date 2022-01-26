5bdb08b73ac2f2c3bc76e279f3d9a27eda33487b
WildboarG <959586@outlook.com>
2022-01-26 22:22:50 +0800

    ...

--- make_study/main.c
+++ make_study/main.c
@@ -1,9 +1,12 @@
 #include "tool1.h"
 #include "tool2.h"
 #include <stdint.h>
+#include <stdio.h>
 
 int main(){
+		printf("This is %s\n",__FUNCTION__);
 		mytool1();
 		mytool2();
+		printf("the end\n");
 		return 0;
 }
--- make_study/tool1.c
+++ make_study/tool1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "tool1.h"
 
+
 void mytool1(void){
 		printf("hello this mytool1\n");
 }
